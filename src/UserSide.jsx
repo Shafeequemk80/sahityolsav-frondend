0c2b2fa912feda38ecc20bed24392c6f0fde1aa8
Shafeequemk80 <shafeequemk80@gmail.com>
2024-07-22 15:38:58 +0530

    75%  complted

--- src/UserSide.jsx
+++ src/UserSide.jsx
@@ -1,36 +1,48 @@
 import React, { useState } from "react";
 import { categories, itemsByCategory } from "./data.js";
 import { getDataServer } from "./context/apiCall.js";
+import html2canvas from "html2canvas";
 
 function UserSide() {
-  const [category, setcategory] = useState("");
-  const [item, setItem] = useState([]);
+  const [category, setCategory] = useState("");
+  const [selectedItem, setSelectedItem] = useState("");
+  const [items, setItems] = useState([]);
   const [results, setResults] = useState(null);
+
   const handleCategoryChange = (event) => {
-    const Category = event.target.value;
-    setItem([]);
-    setcategory(Category);
-    setItem(itemsByCategory[Category]);
-    setResults(null)
+    const selectedCategory = event.target.value;
+    setCategory(selectedCategory);
+    setItems(itemsByCategory[selectedCategory] || []);
+    setResults(null);
   };
 
   const handleItemData = async (event) => {
     const itemValue = event.target.value;
+    setSelectedItem(itemValue);
+    
     try {
       const response = await getDataServer(itemValue, category);
-      
-      console.log(response.data);
-      // Do something with the response, e.g., update state or UI
       setResults(response.data);
-       
     } catch (error) {
       console.error("Error fetching data:", error);
-      // Handle the error, e.g., show an error message to the user
     }
   };
 
-  const scrolltoteam = () => {
-    const element = document.getElementById("results");
+  const handleDownloadImage = async () => {
+    const element = document.getElementById("downloadImage");
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL("image/jpg");
+    const link = document.createElement("a");
+
+    link.href = data;
+    link.download = `${category}-${selectedItem}.jpg`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const scrollToElement = (elementId) => {
+    const element = document.getElementById(elementId);
     if (element) {
       element.scrollIntoView({
         behavior: "smooth",
@@ -43,38 +55,30 @@ function UserSide() {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
     <>
       <div className="bg px-10 xl:px-56 relative text-center w-full h-screen flex flex-col items-center justify-center pt-10 animated">
-        {/* <p className="text-[#fae2e8] text-xl">"India's Largest Chain Event"</p>  */}
-        <h1 className=" text-sm lg:text-xl pb-2 lg:pb-3 md:pb-5 italic">
+        <h1 className="text-sm lg:text-xl pb-2 lg:pb-3 md:pb-5 italic">
           SSF KUNNAMANGALAM DIVISION
         </h1>
         <h1 className="flex flex-col text-center gap-1 xl:gap-2">
-          <span className=" font-extrabold text-4xl lg:text-6xl xl:text-8xl">
+          <span className="font-extrabold text-4xl lg:text-6xl xl:text-8xl">
             Sahithyolsav
           </span>
         </h1>
-        {/* <button className="bg-[#151622] mt-12 rounded-full flex p-3 px-5 text-white">
-      <a className="flex items-center gap-1" target="_blank" href="https://youtube.com">
-        <span className="iconify text-xl" data-icon="mdi:whatsapp"></span>
-        <span className="ml-1 text-lg"> Join Group</span>
-      </a>
-    </button>  */}
-        <div className="pt-14 lg:pt-20 ">
-          <button onClick={scrolltoteam}>
+        <div className="pt-14 lg:pt-20">
+          <button onClick={() => scrollToElement("results")}>
             <span
-              className="iconify text-3xl lg:text-4xl  rounded-full p-1 animate-bounce transition-all duration-500 bg-white"
+              className="iconify text-3xl lg:text-4xl rounded-full p-1 animate-bounce transition-all duration-500 bg-white"
               data-icon="mdi:chevron-down"
             ></span>
           </button>
         </div>
       </div>
-      <div
-        id="results"
-        className="w-full text-center px-10 py-10 lg:py-20 xl:px-56"
-      >
-        <h2 className="py-5 md:py-10 text-4xl lg:text-5xl text-[#e8002c] font-bold ">
+
+      <div id="results" className="w-full text-center px-10 py-10 lg:py-20 xl:px-56">
+        <h2 className="py-5 md:py-10 text-4xl lg:text-5xl text-[#e8002c] font-bold">
           Results
         </h2>
         <div className="flex md:flex-row flex-col md:justify-between space-y-1.5 pt-5 lg:pt-10 md:space-y-0">
@@ -85,9 +89,9 @@ function UserSide() {
               className="p-2 w-full font-medium text-white bg-black rounded md:text-lg md:p-3 md:px-8"
             >
               <option value="">Select Category</option>
-              {categories.map((element) => (
-                <option key={element} value={element}>
-                  {element}
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
                 </option>
               ))}
             </select>
@@ -100,8 +104,8 @@ function UserSide() {
               className="p-2 w-full font-medium text-white bg-black rounded md:text-lg md:p-3 md:px-8"
             >
               <option value="">Select Item</option>
-              {item.map((item) => (
-                <option value={item} key={item}>
+              {items.map((item) => (
+                <option key={item} value={item}>
                   {item}
                 </option>
               ))}
@@ -109,40 +113,60 @@ function UserSide() {
           </div>
         </div>
 
-        <div
-  className="border flex text-black  mt-10 h-96 w-auto"
-  style={{ backgroundImage: "url('/design.jpg')", backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', }}
->
-  {results ? <h1 className="font-400">{results.resultCount}</h1> : ''}
-  {results ? <p>{results.category}</p> : ''}
-  {results ? <p>{results.item}</p> : <p>No results available</p>}
-  {results ? (
-    <div className="text-start">
-      <h2>{results.result[0].firstPrice}</h2>
-      <h5>{results.result[0].firstUnit}</h5>
-      <h2>{results.result[1].secPrice}</h2>
-      <h5>{results.result[1].secUnit}</h5>
-      <h2>{results.result[2].thirdPrice}</h2>
-      <h5>{results.result[2].thirdUnit}</h5>
+        <div className="border flex justify-center text-black mt-10">
+        <figure className="relative max-w-lg" id="downloadImage">
+  <img
+    className="h-auto max-w-full"
+    src="/design.jpg"
+    alt="Background"
+  />
+  {results && (
+    <div className="absolute top-2 left-5 md:left-10 md:top-6 left-5 right-0 bottom-0 flex flex-col p-4">
+      <div className="flex flex-row font-bold">
+        <div className="flex align-middle">
+          <h1 className="text-xl md:pt-2  md:text-2xl lg:text-3xl text-black">
+            {results.resultCount}
+          </h1>
+        </div>
+        <div className="text-start pl-1  md:pl-2">
+          <p className="text-xs md:text-sm lg:text-xl text-black">
+            {results.category}
+          </p>
+          <p className="text-xs md:text-sm lg:text-xl text-black  -mt-1 md:-mt-2" >
+            {results.item}
+          </p>
+        </div>
+      </div>
+      <div className="text-start mt-3">
+        {results.result.map((result, index) => (
+          <div key={index}>
+            <h2 className="text-sm md:text-xl lg:text-2xl text-black roboto-medium">
+              {result.firstPrice || result.secPrice || result.thirdPrice}
+            </h2>
+            <h5 className="text-xs md:text-base lg:text-lg text-black -mt-1 md:-mt-2" >
+              {result.firstUnit || result.secUnit || result.thirdUnit}
+            </h5>
+          </div>
+        ))}
+      </div>
     </div>
-  ) : (
-    ''
   )}
-</div>
+</figure>
 
+        </div>
 
-        
+        <button onClick={handleDownloadImage}>Download</button>
       </div>
 
       <div className="flex flex-col gap-8 py-14 mt-4 text-center w-full px-10 xl:px-56">
-        <h2 className=" text-4xl lg:text-5xl text-[#e8002c] font-bold ">
+        <h2 className="text-4xl lg:text-5xl text-[#e8002c] font-bold">
           What's Sahithyolsav
         </h2>
         <p className="text-lg lg:text-xl font-thin">
           "SSF Kerala's Sahityotsav is an annual literary festival celebrating
           the rich cultural and literary heritage of Kerala, organized by the
           Sunni Students' Federation (SSF) Kerala. The Kunnamangalam division's
-          theme for this year is 'Travel, inviting participants to delve into
+          theme for this year is 'Travel,' inviting participants to delve into
           the literary and cultural dimensions of journeys and voyages. This
           event serves as a vibrant platform for writers, poets, scholars, and
           literary enthusiasts to come together, share their work, and engage in
@@ -157,14 +181,17 @@ function UserSide() {
         <img
           src="/image.jpg"
           className="object-cover h-full w-full rounded-md"
+          alt="Gallery"
         />
         <img
           src="/image.jpg"
           className="object-cover h-full w-full rounded-md"
+          alt="Gallery"
         />
         <img
           src="/image.jpg"
           className="object-cover h-full w-full rounded-md"
+          alt="Gallery"
         />
       </div>
 
@@ -173,25 +200,25 @@ function UserSide() {
           Get connected
         </h1>
         <div className="flex items-center justify-center text-white gap-1">
-          <a target="_blank" href="https://www.instagram.com/ssf_kunnamangalam">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ssf_kunnamangalam">
             <span
               className="text-xl lg:text-3xl iconify"
               data-icon="mdi:instagram"
             ></span>
           </a>
-          <a target="_blank" href="https://www.instagram.com/ssf_kunnamangalam">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/ssf_kunnamangalam">
             <span
               className="text-xl lg:text-3xl iconify"
               data-icon="mdi:facebook"
             ></span>
           </a>
-          <a href="sahithyolsavkgm.vercel.app">
+          <a href="https://sahithyolsavkgm.vercel.app">
             <span
               className="text-xl lg:text-3xl iconify"
               data-icon="mdi:web"
             ></span>
           </a>
-          <a href="https://www.instagram.com/ssf_kunnamangalam">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/ssf_kunnamangalam">
             <span
               className="text-xl lg:text-3xl iconify"
               data-icon="mdi:youtube"
@@ -199,16 +226,16 @@ function UserSide() {
           </a>
         </div>
         <h1 className="text-gray-100 mt-4 mb-2">
-          <span className="text-xl">&copy;</span> - 2023
+          <span className="text-xl">&copy;</span> 2023
         </h1>
       </footer>
 
       <button
         onClick={scrollToTop}
-        className="flex items-center justify-center z-50 fixed bottom-10 right-10  p-2 bg-[#e8002c] text-white rounded-full"
+        className="flex items-center justify-center z-50 fixed bottom-10 right-10 p-2 bg-[#e8002c] text-white rounded-full"
       >
         <span
-          className="iconify text-xl lgtext-2xl"
+          className="iconify text-xl lg:text-2xl"
           data-icon="mdi:arrow-up"
         ></span>
       </button>
